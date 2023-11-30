66088c95f28dcfa2418a56dc86cd3d7c17eb7b05
Pavan kumar <135860651+pavan347@users.noreply.github.com>
2023-11-30 09:49:18 +0530

    Delete LoginProject directory

--- LoginProject/src/main/java/com/database/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.database;
-
-public interface UserDAO {
-	
-	boolean checkLogin(String email,String password);
-	
-	boolean createUser(String name,String email,String password);
-	
-}
--- LoginProject/src/main/webapp/javascript/websiteJS.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.menu-icon').addEventListener('click', function () {
-    document.querySelector('nav').classList.toggle('show-menu');
-  });
-
-  function toggleDescription(element) {
-    const description = element.nextElementSibling;
-    description.style.display = description.style.display === 'block' ? 'none' : 'block';
-  }
